513be14c2e9aa03bf6bf81e601c4feb174cd4426
Uilian Ries <uilianries@gmail.com>
2019-02-03 21:24:25 -0200

    Add parse step
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- main.cc
+++ main.cc
@@ -7,7 +7,20 @@ int main() {
     sensor.set_temperature(23.4);
     sensor.set_humidity(68);
     sensor.set_door(Sensor_SwitchLevel_OPEN);
-    std::cout << "Serialized data: " << sensor.SerializeAsString() << std::endl;
+
+    const std::string serialized_sensor = sensor.SerializeAsString();
+
+    std::cout << "Serialized data: " << serialized_sensor << std::endl;
+
+    {
+	 Sensor sensor;
+	 sensor.ParseFromString(serialized_sensor);
+	 std::cout << "Name: " << sensor.name() << std::endl;
+	 std::cout << "Temperature: " << sensor.temperature() << std::endl;
+	 std::cout << "Humidity: " << sensor.humidity() << std::endl;
+	 std::cout << "Door is: " << (sensor.door() ? "Open" : "Closed") << std::endl;
+    }
+
     return 0;
 }
 
